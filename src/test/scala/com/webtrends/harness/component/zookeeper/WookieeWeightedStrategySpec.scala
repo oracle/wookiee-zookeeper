28bb0a0739890088a42376729ecc16ddc65dab8f
Robert Towne <robert.towne@webtrends.com>
2016-09-07 08:44:36 -0700

    add update test

--- src/test/scala/com/webtrends/harness/component/zookeeper/WookieeWeightedStrategySpec.scala
+++ src/test/scala/com/webtrends/harness/component/zookeeper/WookieeWeightedStrategySpec.scala
@@ -15,29 +15,28 @@ class WookieeWeightedStrategySpec extends SpecificationWithJUnit with NoTimeConv
     override def getInstances: util.List[ServiceInstance[WookieeServiceDetails]] = instances.toList.asJava
   }
 
+  def builderInstance(id: Int, weight: Int) =  ServiceInstance.builder[WookieeServiceDetails]()
+    .uriSpec(new UriSpec(s"akka.tcp://server@localhost:8080/"))
+    .id(id.toString)
+    .name(UUID.randomUUID().toString)
+    .payload(new WookieeServiceDetails(weight))
+    .port(8080)
+    .build()
+
+
   "WookieeWeightedStrategy" should {
-    val builder = ServiceInstance.builder[WookieeServiceDetails]()
-      .uriSpec(new UriSpec(s"akka.tcp://server@localhost:8080/"))
 
     "returns null when no instances" in {
       val instances = Seq.empty[ServiceInstance[WookieeServiceDetails]]
-
       val instanceProvider = new MockInstanceProvider(instances)
       val strategy = new WookieeWeightedStrategy()
 
       strategy.getInstance(instanceProvider) mustEqual null
     }
 
-    "default to round-robin when weights are all the same" in {
-      val instances = (0 to 10).map { index =>
-        builder
-          .id(index.toString)
-          .name(UUID.randomUUID().toString)
-          .payload(new WookieeServiceDetails(0))
-          .port(8080)
-          .build()
-      }
 
+    "default to round-robin when weights are all the same" in {
+      val instances = (0 to 10).map(i => builderInstance(i, 0))
       val instanceProvider = new MockInstanceProvider(instances)
       val strategy = new WookieeWeightedStrategy()
 
@@ -45,24 +44,30 @@ class WookieeWeightedStrategySpec extends SpecificationWithJUnit with NoTimeConv
     }
 
     "pick the lowest weighted instance" in {
-      val instances = (1 to 10).map { index =>
-        builder
-          .id(index.toString)
-          .name(UUID.randomUUID().toString)
-          .payload(new WookieeServiceDetails(index))
-          .port(8080)
-          .build()
-      } ++ Seq(builder
-              .id("0")
-              .name(UUID.randomUUID().toString)
-              .payload(new WookieeServiceDetails(0))
-              .port(8080)
-              .build())
-
+      val instances = (1 to 10).map(i => builderInstance(i,i)) ++ Seq(builderInstance(0,0))
       val instanceProvider = new MockInstanceProvider(instances)
       val strategy = new WookieeWeightedStrategy()
 
       strategy.getInstance(instanceProvider).getId mustEqual "0"
     }
+
+
+
+    "pick the lowest as weight changes" in {
+      val instances = (10 to 20).map(i => builderInstance(i,i)) ++ Seq( builderInstance(5,5))
+      val instanceProvider = new MockInstanceProvider(instances)
+
+      val strategy = new WookieeWeightedStrategy()
+
+      // first check prior to updated instance weights has lowest 5
+      strategy.getInstance(instanceProvider).getId mustEqual "5"
+
+
+      // second check after weight for instance 5 has increased and now id 10 is lowest
+      val updatedInstances = (10 to 20).map(i => builderInstance(i,i)) ++ Seq( builderInstance(5, 15))
+      val updatedProvider = new MockInstanceProvider(updatedInstances)
+      strategy.getInstance(updatedProvider).getId mustEqual "10"
+    }
+
   }
 }
