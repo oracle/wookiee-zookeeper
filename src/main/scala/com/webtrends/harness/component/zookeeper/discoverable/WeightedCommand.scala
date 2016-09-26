98746f1e30d66a0e7222f3a5e254353bd1f6f777
Scott Totten <scott.totten@webtrends.com>
2016-09-26 15:31:39 -0700

    modified weight command

--- src/main/scala/com/webtrends/harness/component/zookeeper/discoverable/WeightedCommand.scala
+++ src/main/scala/com/webtrends/harness/component/zookeeper/discoverable/WeightedCommand.scala
@@ -4,7 +4,7 @@ import com.webtrends.harness.command.Command
 
 import scala.concurrent.Future
 
-case class Weight(weight: Int)
+case class Weight(weight: Int, forcedSet: Boolean)
 
 trait WeightedCommand extends Command {
   this: Discoverable =>
@@ -12,9 +12,10 @@ trait WeightedCommand extends Command {
   val basePath: String
   val commandId: String
 
-  def updateWeight(weight: Int): Future[Boolean] = updateWeight(weight, basePath, commandName, commandId)
+  def updateWeight(weight: Int, forcedSet: Boolean): Future[Boolean] =
+    updateWeight(weight, basePath, commandName, commandId, forcedSet)
 
   override def receive =({
-    case Weight(w) => updateWeight(w)
+    case Weight(w, f) => updateWeight(w, f)
   }: Receive) orElse super.receive
 }
